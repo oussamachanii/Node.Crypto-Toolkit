cb3cd64bc69460bdc761f36b0dd4665727bf6bd4
Andi Dittrich <andi@andidittrich.de>
2017-04-14 12:04:04 +0000

    added hash output type base64-urlsafe; added random hash generator

--- CHANGES.md
+++ CHANGES.md
@@ -1,2 +1,7 @@
+### 1.1.0 ###
+* Added: hash output type `base64-urlsafe`
+* Added: hash-input can be of type **Buffer** 
+
+
 ### 1.0.0 ###
 Initial public release
--- crypto-toolkit.js
+++ crypto-toolkit.js
@@ -1,6 +1,8 @@
 var _hash = require('./lib/Hash');
+var _randomhash = require('./lib/RandomHash');
 
 // expose toolkit
 module.exports = {
-    Hash: _hash
+    Hash: _hash,
+    RandomHash: _randomhash
 };
